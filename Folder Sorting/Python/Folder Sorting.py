54d90245e0e5e88991a2921d28250a14299b1e0a
Okiki-28 <153521252+Okiki-28@users.noreply.github.com>
2025-01-23 20:05:00 +0000

    Updated Folder Sorting Python file to make it more user friendly and usable by public

--- Folder Sorting/Python/Folder Sorting.py
+++ Folder Sorting/Python/Folder Sorting.py
@@ -1,17 +1,19 @@
 import os
 import re
+from pathlib import Path
 
-path = r'C:\Users\olume\Downloads'
+path = Path.home() / "Documents"
 target_path = r'C:\Users\olume\Documents\Moved Downloads'
 
 def get_folder(ext):
+    
     text_files = [".txt", ".pdf", ".odt", ".rtf"]
     office_files = [".pptm", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt", ".ods", ".odp"]
     image_files = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg", ".webp"]
     audio_files = [".mp3", ".wav", ".aac", ".flac", ".ogg"]
     video_files = [".mp4", ".mkv", ".avi", ".mov", ".wmv"]
     compressed_files = [".zip", ".rar", ".7z", ".tar.gz"]
-    executable_and_script_files = [".exe", ".bat", ".sh", ".py", ".jar"]
+    executable_and_script_files = [".bat", ".sh", ".py", ".jar"]
     web_files = [".html", ".htm", ".css", ".js", ".php", ".json"]
     database_files = [".sql", ".db", ".sqlite", ".csv", ".xls", ".xlsx"]
     application_files = [".exe", ".msi", ".bat", ".sh", ".jar", ".apk", ".app", ".dmg", ".iso", ".deb", ".rpm"]
@@ -40,7 +42,39 @@ def get_folder(ext):
     else:
         return False
 
-def folder(path, target_path):
+def folder():
+    arr = ["Desktop", "Documents", "Downloads"]
+    #Choosing Path
+    for a in range(len(arr)):
+        print(a+1, arr[a])
+    i = input("Choose folder you wish to sort files [1-3]: ")
+    while True:
+        if not i.isdigit():
+            print("Invalid")
+            continue
+        elif not 1 <= int(i) <= len(arr):
+            print("Invalid")
+            continue
+        break
+    i = int(i)-1
+    path = Path.home() / arr[i]
+    print(f"Initial path: {path}")
+
+    #Choosing Destination
+    home= input("Enter name of folder to store document files: ")
+    i1 = input("Choose home folder from list above to store this [1-3]: ")
+    while True:
+        if not i1.isdigit():
+            print("Invalid")
+            continue
+        elif not 1 <= int(i1) <= len(arr):
+            print("Invalid")
+            continue
+        break
+    i1 = int(i1)-1
+    target_path = os.path.join(Path.home(), arr[i1], home)
+    print("Target Path: {target_path}")
+
     files = os.listdir(path)
 
     for file in files:
@@ -60,8 +94,11 @@ def folder(path, target_path):
 
             if not os.path.exists(new_folder):
                 os.makedirs(new_folder)
+                print("New folder created!\n")
+            else:
+                print("Folder exists!\n")
 
             print(old_path, '>>', new_path)
             os.rename(old_path, new_path)
         
-folder(path, target_path)
+folder()
